f0ece84e3edc9b15017b873ea79eb8ddbd8270cb
andrew alton <joeblk@gmail.com>
2021-10-05 21:51:24 -0800

    Updated with App.css and App.jsx files

---
+++ src/components/App.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import './App.css'
+import TitleBar from './TitleBar/TitleBar';
+
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.books = [
+            {title: "The Old Man and the Sea", author: "Ernest Hemingway"},
+            {title: 'All the Light We Cannot See', author: 'Anthony Doerr'},
+            {title: 'The First and Last Freedom', author: 'Jiddu Krishnamurit'},
+        ];
+        this.state = {
+            bookNumber: 0
+        };
+    }
+
+    render(){
+        return (
+        <div className="container-fluid">
+            <TitleBar/>
+            <div className="col-md-4">
+                {/*Button here to move to the previous book viewed*/}
+            </div>
+            <div className="col-md-4">
+                {/*Display book with cover here*/}
+                <h1>{this.books[this.state.bookNumber].title}</h1>
+                <h4
+            </div> 
+            <div className="col-md-4"></div>
+                {/*Button here to move to the next book viewed*/}  
+        </div>    
+        )
+    }
+}
+
+export default App;
